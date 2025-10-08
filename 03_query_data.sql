85986703844a1cef58647698b28882dc1769b489
tacodepastor25 <125671857+tacodepastor25@users.noreply.github.com>
2025-10-07 19:26:00 -0600

    Update 03_query_data.sql

--- 03_query_data.sql
+++ 03_query_data.sql
@@ -1,1 +1,21 @@
+SELECT id, name, metadata->>'brand' AS brand
+FROM data_types_demo
+WHERE metadata ? 'brand';
 
+SELECT id, name, launch_date
+FROM data_types_demo
+WHERE launch_date > '2025-09-20';
+
+SELECT id, name, tags
+FROM data_types_demo
+WHERE 'tecnología' = ANY(tags);
+
+SELECT id, name, ip_address, mac_address
+FROM data_types_demo;
+
+SELECT 
+    id, 
+    name, 
+    ST_X(coordinates) AS longitude, 
+    ST_Y(coordinates) AS latitude
+FROM data_types_demo;
