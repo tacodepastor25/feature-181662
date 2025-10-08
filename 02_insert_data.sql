4d12bdcacdf98f5339f67e1b531834b309a87d6c
tacodepastor25 <125671857+tacodepastor25@users.noreply.github.com>
2025-10-07 19:22:46 -0600

    Update 02_insert_data.sql

--- 02_insert_data.sql
+++ 02_insert_data.sql
@@ -1,1 +1,17 @@
-
+INSERT INTO data_types_demo 
+(name, description, price, discount, available, launch_date, ip_address, mac_address, tags, metadata, coordinates, file_data)
+VALUES 
+(
+    'Xiaomi RedmiNote', 
+    'Smarthphone Gama Media', 
+    2800.00, 
+    0.10, 
+    TRUE, 
+    '2023-04-15', 
+    '192.168.0.001', 
+    '01:10:12:34:56:78', 
+    ARRAY['Samsung', 'Huawei', 'Apple'], 
+    '{"brand": "Mac", "Peso": "Negro", "Camara": "Memoria"}', 
+    ST_SetSRID(ST_MakePoint(-100.9847, 22.1498), 4326), 
+    decode('42696e61727944617461', 'hex')  -- "BinaryData" en hex
+);
