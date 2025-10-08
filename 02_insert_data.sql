8a148f0689d4db0f2702c7e621a5456e22ec21c1
tacodepastor25 <125671857+tacodepastor25@users.noreply.github.com>
2025-10-07 21:43:28 -0600

    Update 02_insert_data.sql

--- 02_insert_data.sql
+++ 02_insert_data.sql
@@ -1,34 +1,34 @@
+-- insert
 INSERT INTO data_types_demo (
-    name, description, price, discount, available, launch_date, uuid_col, ip_address, mac_address, tags, metadata, coordinates, file_data
+    name, description, price, discount, available, launch_date,
+    uuid_col, ip_address, mac_address, tags, metadata, coordinates, file_data
 ) VALUES (
-    'Xiaomi RedmiNote',
-    'Smarthphone Gama Media',
-    2800.00,
-    0.10,
-    TRUE,
-    '2023-04-15',
-    '123e4567-e89b-12d3-a456-426614174000',             
-    '192.168.0.1',                                     
-    '01:10:12:34:56:78',
-    ARRAY['Samsung', 'Huawei', 'Apple'],
-    '{"brand": "Mac", "Color": "Negro", "Memoria": "Expandible"}',
-    '(-100.9847, 22.1498)',                             
-    E'\\x42696e61727944617461'                         
-),
-(
-    'Iphone 17',
-    'Smarthphone Gama Alta',
-    18500.50,
-    0.15,
-    TRUE,
-    '2025-10-01',
-    '223e4567-e89b-12d3-a456-426614174001',
-    '10.0.0.45',
-    '00:1A:2B:3C:4D:5E',
-    ARRAY['Samsung', 'Huawei', 'Apple'],
-    '{"brand": "CompuMax", "Color": "gris", "Garanmtia": "2 años"}',
-    '(-99.1332, 19.4326)',
-    E'\\x4C6170746F7042696E617279'                      
+    'Producto 1', 'Producto de prueba', 199.99, 10.5,
+    TRUE, '2025-10-01', gen_random_uuid(), '192.168.1.10', '08:00:27:13:69:77',
+    ARRAY['nuevo', 'promoción'], '{"color": "negro", "peso": "2.5kg"}',
+    '(10.5, 20.3)', decode('97656c6c6j20576f726c64', 'hex')  
+);
+
+INSERT INTO data_types_demo (
+    name, description, price, discount, available, launch_date,
+    uuid_col, ip_address, mac_address, tags, metadata, coordinates, file_data
+) VALUES (
+    'Producto 2', 'Producto de prueba num 2',499.99, 10.0, TRUE, 
+    '2025-11-01', gen_random_uuid(), '192.168.100.25', '00:1A:2B:3C:4D:5E',
+    ARRAY['electrónica', 'marca premium'],
+    '{"brand": "Tecnología", "color": "blanco", "almacenamiento": "64GB"}',
+    '(25.3, 42.7)', decode('536d61727470686f6e652064617461', 'hex')  -- "Smartphone data"
+);
+
+INSERT INTO data_types_demo (
+    name, description, price, discount, available, launch_date,
+    uuid_col, ip_address, mac_address, tags, metadata, coordinates, file_data
+) VALUES (
+    'Producto 3', 'Producto de prueba num 3', 1699.50, 20.0, FALSE,
+    '2025-12-10', gen_random_uuid(), '10.10.10.10', 'AA:BB:CC:DD:EE:FF',
+    ARRAY['computación', 'descuento'],
+    '{"brand": "CompuMax", "procesador": "AMD 3", "RAM": "32GB"}',
+    '(12.0, 33.5)', decode('4c6170746f702064617461', 'hex')  -- "Laptop data"
 );
 
 
